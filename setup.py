9dd5a9fbdaf67b996124435ebe388b55c4ff6357
noname115 <robert.kolcun@gmail.com>
2020-11-04 16:59:42 +0100

    Fix requirements & package-version

--- setup.py
+++ setup.py
@@ -1,51 +1,57 @@
 # -*- coding: UTF-8 -*-
 
+import os
 from setuptools import setup
+from typing import List
 
-# http://stackoverflow.com/a/7071358/735926
-import re
-VERSIONFILE='drive/__init__.py'
-verstrline = open(VERSIONFILE, 'rt').read()
-VSRE = r'^__version__\s+=\s+[\'"]([^\'"]+)[\'"]'
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    verstr = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % VERSIONFILE)
 
-setup(
-    name='drive',
-    version=verstr,
-    author='Baptiste Fontaine',
-    author_email='baptiste.fontaine@oscaro.com',
-    packages=['drive', 'drive.cli'],
-    url='https://github.com/oscaro/drive',
-    license='MIT License',
-    description='Google Drive client',
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',
-    install_requires=[
-        "google-api-python-client==1.5.3",
-        "httplib2==0.18.0",
-        "oauth2client==3.0.0",
-        "openpyxl==2.4.0",
-        "pyasn1==0.1.9",
-        "pyasn1-modules==0.0.8",
-        "python-magic==0.4.12",
-        "rsa==3.4.2",
-        "simplejson==3.8.2",
-        "six==1.10.0",
-        "uritemplate==0.6",
-    ],
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    entry_points={
-        "console_scripts": [
-            "gd-upload=drive.cli.upload:main",
-            "gd-download=drive.cli.download:main",
-        ]
-    }
-)
+def _get_relative_path(file_path: str) -> str:
+    return os.path.join(os.path.dirname(__file__), file_path)
+
+def load_requirements() -> List[str]:
+    # Load requirements
+    requirements = []  # type: List[str]
+    with open(_get_relative_path("requirements.txt"), "r") as req_file:
+        lines = [line.rstrip("\n") for line in req_file]
+        lines = list(filter(lambda line: line != "" and line[0] != "#", lines))
+        for line in lines:
+            hash_pos = line.find("#")
+            if hash_pos != -1:
+                requirements.append(line[:hash_pos].strip())
+            else:
+                requirements.append(line)
+    return requirements
+
+
+def main():
+    drive_ns = {}
+    with open(_get_relative_path("drive/__version__.py")) as f:
+        exec(f.read(), drive_ns)
+
+    setup(
+        name='drive',
+        version=drive_ns["__version__"],
+        author='Baptiste Fontaine',
+        author_email='baptiste.fontaine@oscaro.com',
+        packages=['drive', 'drive.cli'],
+        url='https://github.com/NoName115/drive',
+        license='MIT License',
+        description='Google Drive client',
+        long_description=open('README.md', encoding='utf-8').read(),
+        long_description_content_type='text/markdown',
+        install_requires=load_requirements(),
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+        ],
+        entry_points={
+            "console_scripts": [
+                "gd-upload=drive.cli.upload:main",
+                "gd-download=drive.cli.download:main",
+            ]
+        }
+    )
+
+if __name__ == "__main__":
+    main()
