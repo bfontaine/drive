3fb8ca67cd773a9bf6d2d667d2bc8872a4f9ca07
Baptiste Fontaine <baptiste@bixoto.com>
2021-06-02 11:08:18 +0200

    formatting

--- drive/__init__.py
+++ drive/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: UTF-8 -*-
 
-__version__ = "0.3.2"
-
 from drive.client import Client
 
-_ = Client  # Silent Pyflakes
+__version__ = "0.3.2"
+
+__all__ = [
+    "__version__",
+    "Client",
+]
