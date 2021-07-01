1620d224239816dbfe19c849eaaccf404520f76e
Róbert Kolcún <robert.kolcun@artin.cz>
2021-07-01 09:21:57 +0200

    Update download & upload cli scripts

--- drive/cli/download.py
+++ drive/cli/download.py
@@ -1,28 +1,87 @@
+import os
 import argparse
-from drive.client import Client
+from typing import List
+from drive.client import Client, File
+
+
+def _download(
+    client: Client,
+    file_to_download: File,
+    output_folder: str,
+    recursive: bool,
+    root_folder: bool = False,
+):
+    if file_to_download.is_directory:
+        if not recursive and not root_folder:
+            print(f"Recursive download not enabled, skipping '{file_to_download.name}' folder...")
+            return
+
+        print(f"Downloading folder: {file_to_download.name}/")
+        if not root_folder:
+            os.makedirs(os.path.join(output_folder, file_to_download.name), exist_ok=True)
+
+        for file in file_to_download.list():
+            _download(
+                client=client,
+                file_to_download=file,
+                output_folder=output_folder if root_folder else os.path.join(output_folder, file_to_download.name),
+                recursive=recursive
+            )
+    else:
+        print(f"Downloading: {file_to_download.name}")
+        file_to_download.download_file(f"{output_folder}/{file_to_download.name}")
+        print("  Done")
+
+
+def main_download(
+    files_to_download: List[str],
+    output_folder: str,
+    recursive: bool
+):
+    c = Client()
+    for file_id in files_to_download:
+        gf = c.get_file(
+            file_id=file_id,
+            supports_all_drives=True
+        )
+        _download(
+            client=c,
+            file_to_download=gf,
+            output_folder=output_folder,
+            recursive=recursive,
+            root_folder=True
+        )
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Script for download file or folder")
-    parser.add_argument("--input", "-i", help="Target file/folder to download", required=True)
-    parser.add_argument("--output", "-o", help="Output folder to store downloaded files", required=True)
+    parser = argparse.ArgumentParser(
+        description="Script for download file or folder"
+    )
+    parser.add_argument(
+        "--input", "-i",
+        nargs="+",
+        help="Target file/folder to download",
+        required=True
+    )
+    parser.add_argument(
+        "--output", "-o",
+        help="Output folder to store downloaded files",
+        required=True
+    )
+    parser.add_argument(
+        "--recursive", "-r",
+        type=bool,
+        help="If set to True, it downloads recursively",
+        default=False
+    )
     args = parser.parse_args()
 
-    c = Client()
-    d = c.get_file(
-        file_id=args.input,
-        supports_all_drives=True
+    main_download(
+        files_to_download=args.input,
+        output_folder=args.output,
+        recursive=bool(args.recursive)
     )
 
-    if d.is_directory:
-        for file_to_download in d.list():
-            print(f"Downloading: {file_to_download.name}")
-            file_to_download.download_file(f"{args.output}/{file_to_download.name}")
-            print(f"  Done")
-    else:
-        print(f"Downloading: {d.name}")
-        d.download_file(f"{args.output}/{d.name}")
-        print("  Done")
 
 if __name__ == "__main__":
     main()
--- drive/cli/upload.py
+++ drive/cli/upload.py
@@ -1,26 +1,55 @@
 import argparse
+from typing import List
 from drive.client import Client
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Script for uploading file into folder")
-    parser.add_argument("--output", "-o", help="Target folder identifier.", required=True)
-    parser.add_argument("--file", "-f", nargs="+", help="Files to upload", required=True)
-    parser.add_argument("--update-existing", "-U", help="Update existing files instead of uploading new ones.",
-                        default=False)
-    args = parser.parse_args()
+def main_upload(
+    target_folder: str,
+    files_to_upload: List[str],
+    update_file: bool
+):
+    if not files_to_upload:
+        return
 
     c = Client()
-
-    for file_to_upload in args.file:
-        print(f"Uploading file: {file_to_upload}")
+    for file_path in files_to_upload:
+        print(f"Uploading file... {file_path}")
         new_file = c.upload_file(
-            parent_id=args.output,
-            path=file_to_upload,
-            update_existing=args.update_existing,
+            parent_id=target_folder,
+            path=file_path,
+            update_existing=update_file,
             supports_all_drives=True
         )
         print(f"  Done: {new_file}")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script for uploading file into folder"
+    )
+    parser.add_argument(
+        "--output", "-o",
+        help="Target folder identifier",
+        required=True
+    )
+    parser.add_argument(
+        "--file", "-f",
+        nargs="+",
+        help="Files to upload",
+        required=True
+    )
+    parser.add_argument(
+        "--update-existing", "-U",
+        help="Update existing files instead of uploading new ones",
+        default=False
+    )
+    args = parser.parse_args()
+
+    main_upload(
+        target_folder=args.output,
+        files_to_upload=args.file,
+        update_file=bool(args.update_exsiting)
+    )
 
 
 if __name__ == "__main__":
